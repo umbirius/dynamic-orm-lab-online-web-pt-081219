6c23ddd79cc1324f98637f70f3ee9bfdf931fd6f
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-24 22:49:55 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -28,7 +28,7 @@ class InteractiveRecord
 
 
   def initialize(options={})
-    self.column_names.each do |col_name|
+    self.class.column_names.each do |col_name|
       attr_accessor col_name.to_sym
     end
     options.each do |property, value|
